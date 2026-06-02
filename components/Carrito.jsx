efbd3612ff8c8b72a7845004ed1e75863eac2216
Francisco Dávila <fjhdavila@gmail.com>
2026-06-02 20:52:14 +0100

    feat: Se actualiza lógica de componentes

--- components/Carrito.jsx
+++ components/Carrito.jsx
@@ -4,6 +4,8 @@ const Carrito = () => {
   return (
     <div>
       <h1>En rama componentes</h1>
+      <h2>Carrito</h2>
+      <p>Carrito de compras</p>
     </div>
   );
 };
--- components/Producto.jsx
+++ components/Producto.jsx
@@ -4,6 +4,8 @@ const Producto = () => {
   return (
     <div>
       <h1>En rama componentes</h1>
+      <h2>Producto</h2>
+      <p>Listado de productos</p>
     </div>
   );
 };
