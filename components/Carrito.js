7a72af8949942b0d861a3a581ffc93dcc4527406
Francisco Dávila <fjhdavila@gmail.com>
2026-06-02 20:09:31 +0100

    feat: Se recuperan componentes desde historial

---
+++ components/Carrito.js
@@ -0,0 +1,12 @@
+// Carrito.js
+import React from "react";
+
+const Carrito = () => {
+  return (
+    <div>
+      <h1>Carrito</h1>
+    </div>
+  );
+};
+
+export default Carrito;
---
+++ components/Producto.js
@@ -0,0 +1,12 @@
+// Producto.js
+import React from "react";
+
+const Producto = () => {
+  return (
+    <div>
+      <h1>Producto</h1>
+    </div>
+  );
+};
+
+export default Producto;
