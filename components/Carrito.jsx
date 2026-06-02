93bb11eb5b6a15b6ccbeeae1492a2c2928724104
Francisco Dávila <fjhdavila@gmail.com>
2026-06-02 20:45:22 +0100

    feat: Se desarrolla lógica de Carrito y Producto

---
+++ components/Carrito.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Carrito = () => {
+  return (
+    <div>
+      <h1>En rama componentes</h1>
+    </div>
+  );
+};
+
+export default Carrito;
---
+++ components/Producto.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Producto = () => {
+  return (
+    <div>
+      <h1>En rama componentes</h1>
+    </div>
+  );
+};
+
+export default Producto;
